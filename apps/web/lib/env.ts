71316e43d72c6789d540169f5a717c7c79bec030
Love Akinlesi <akinlesil@gmail.com>
2025-10-31 11:30:53 +0000

    refactor(env): remove unused environment variables from configuration

--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -5,14 +5,6 @@ export const env = zennv({
   dotenv: true,
   schema: z.object({
     NODE_ENV: z.string().default("development"),
-    PG_SSL: z.string().default("true"),
-    DB_HOST: z.string(),
-    DB_PORT: z.string(),
-    DB_NAME: z.string(),
-    DB_USERNAME: z.string(),
-    DB_PASSWORD: z.string(),
     DATABASE_URL: z.string(),
-    JWT_SECRET: z.string().default("your-secret-key"),
-    JWT_REFRESH_SECRET: z.string().default("your-refresh-secret"),
   }),
 });
