e32a9b1edfce85d2cf94a9114110249dd8c26d41
Love Akinlesi <akinlesil@gmail.com>
2025-11-06 12:44:55 +0000

    refactor: Remove unused authentication logic and clean up UserAvatar component

--- apps/web/components/landing/user-avatar.tsx
+++ apps/web/components/landing/user-avatar.tsx
@@ -1,15 +1,13 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Fbutton } from "@/components/ui/fancy-button";
+import Link from "next/link";
 
 export default async function UserAvatar() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  // const session = await auth.api.getSession({
+  //   headers: await headers(),
+  // });
 
-  if (!session) {
+
     return (
         <Link href="/auth/login">
       <Fbutton size="sm"  variant="secondary">
@@ -17,15 +15,15 @@ export default async function UserAvatar() {
       </Fbutton>
         </Link>
     );
-  }
 
-  return (
-    <Avatar className="rounded-lg size-9 md:size-10 border-2 border-foreground">
-      <AvatarImage
-        src={"https://cdn.jsdelivr.net/gh/alohe/avatars/png/upstream_20.png"}
-        alt={"LoveAkinlesi"}
-      />
-      <AvatarFallback>LA</AvatarFallback>
-    </Avatar>
-  );
+
+  // return (
+  //   <Avatar className="rounded-lg size-9 md:size-10 border-2 border-foreground">
+  //     <AvatarImage
+  //       src={"https://cdn.jsdelivr.net/gh/alohe/avatars/png/upstream_20.png"}
+  //       alt={"LoveAkinlesi"}
+  //     />
+  //     <AvatarFallback>LA</AvatarFallback>
+  //   </Avatar>
+  // );
 }
